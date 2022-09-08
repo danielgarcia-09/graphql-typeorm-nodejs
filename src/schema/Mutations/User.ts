eba1e56f39f9eb1bf50e6e20cbdc4ff3a6e2a0e0
Daniel Garcia <daniel.garcia7913@gmail.com>
2022-09-08 10:06:32 -0400

    adding input for user update

--- src/schema/Mutations/User.ts
+++ src/schema/Mutations/User.ts
@@ -1,7 +1,13 @@
-import { GraphQLBoolean, GraphQLID, GraphQLString } from "graphql";
+import {
+  GraphQLBoolean,
+  GraphQLID,
+  GraphQLInputObjectType,
+  GraphQLString,
+} from "graphql";
 import { User } from "../../Entities/User";
 import { UserType } from "../typeDefs/User";
-import bcrypt from 'bcryptjs';
+import bcrypt from "bcryptjs";
+import { MessageType } from "../typeDefs/Message";
 
 export const CREATE_USER = {
   type: UserType,
@@ -31,47 +37,70 @@ export const CREATE_USER = {
 export const DELETE_USER = {
   type: GraphQLBoolean,
   args: {
-    id: {type: GraphQLID}
+    id: { type: GraphQLID },
   },
   async resolve(parent: any, { id }: any) {
     try {
-      const result = await User.delete({ id })
-      return result.affected === 1 ? true : false;      
-    } catch (error) {
-      return error
-    }
-  }
-}
-
-export const UPDATE_USER = {
-  type: GraphQLBoolean,
-  args: {
-    id: { type: GraphQLID },
-    name: { type: GraphQLString },
-    username: { type: GraphQLString },
-    oldPassword: { type: GraphQLString },
-    password: { type: GraphQLString },
-  },
-  async resolve( parent: any, { id, name, username, oldPassword, password }: any) {
-    try {
-      const userFound = await User.findOne(id) as User;
-      
-      const isMatch = await bcrypt.compare(oldPassword, userFound.password);
-
-      if(!isMatch) return false;
-
-      const newPassword = await bcrypt.hash(password, 10);
-      
-      const result = await User.update({id}, {
-        name,
-        username,
-        password : newPassword
-      });
-
+      const result = await User.delete({ id });
       return result.affected === 1 ? true : false;
-      
     } catch (error) {
       return error;
     }
-  }
-}
+  },
+};
+
+export const UPDATE_USER = {
+  type: MessageType,
+  args: {
+    id: { type: GraphQLID },
+    input: {
+      type: new GraphQLInputObjectType({
+        name: "UserInput",
+        fields: {
+          name: { type: GraphQLString },
+          username: { type: GraphQLString },
+          oldPassword: { type: GraphQLString },
+          password: { type: GraphQLString },
+        },
+      }),
+    },
+  },
+  async resolve(
+    parent: any,
+    { id, input }: any
+  ) {
+    try {
+      const {name, username, oldPassword, password} = input;
+
+      const userFound = (await User.findOneBy({ id })) as User;
+
+      if (!userFound) return { success: false, message: "User not found" };
+
+      const isMatch = await bcrypt.compare(oldPassword, userFound.password);
+
+      if (!isMatch) return { success: false, message: "Wrong password" };
+
+      const newPassword = await bcrypt.hash(password, 10);
+
+      const result = await User.update(
+        { id },
+        {
+          name,
+          username,
+          password: newPassword,
+        }
+      );
+
+      const isUpdated = result.affected === 1;
+
+      return {
+        success: isUpdated ? true : false,
+        message: isUpdated
+          ? "User updated successfully"
+          : "Error with user update, retry :(",
+      };
+    } catch (error) {
+      return error;
+    }
+  },
+};
